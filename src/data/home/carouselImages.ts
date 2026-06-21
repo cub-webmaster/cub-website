831d99f1256187e2e9a5921bdf423e878b67f8f1
rachleona <ractam2002@gmail.com>
2026-06-21 19:58:39 +0100

    Added preview and check actions + ran linting

--- src/data/home/carouselImages.ts
+++ src/data/home/carouselImages.ts
@@ -1,19 +1,19 @@
-import type { CarouselImage } from "$lib/carousel";
-import image1 from '$lib/assets/img/2019Team.jpg'
-import image2 from '$lib/assets/img/BUTTSChamps2020.jpg'
-import image3 from '$lib/assets/img/CameronPodium.jpg'
+import type { CarouselImage } from '$lib/carousel';
+import image1 from '$lib/assets/img/2019Team.jpg';
+import image2 from '$lib/assets/img/BUTTSChamps2020.jpg';
+import image3 from '$lib/assets/img/CameronPodium.jpg';
 
 export const homeImages: CarouselImage[] = [
-      {
-         path: image1,
-         alt:  "Varsity 2019 team photo"
-      },
-      {
-         path: image2,
-         alt:  "BUTTS Championship 2020"
-      },
-      {
-         path: image3,
-         alt:  "Cameron Krulewski's Gold (Ladies Barebow) BUCS 2020"
-      },
-   ]
+	{
+		path: image1,
+		alt: 'Varsity 2019 team photo'
+	},
+	{
+		path: image2,
+		alt: 'BUTTS Championship 2020'
+	},
+	{
+		path: image3,
+		alt: "Cameron Krulewski's Gold (Ladies Barebow) BUCS 2020"
+	}
+];
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	plugins: [sveltekit(), tailwindcss()],
 	resolve: {
-        alias: {
-            "$components": resolve("./src/components/"),
-            "$data": resolve("./src/data/")
-        }
-    },
-
+		alias: {
+			$components: resolve('./src/components/'),
+			$data: resolve('./src/data/')
+		}
+	}
 });
