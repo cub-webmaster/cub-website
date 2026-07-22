729b74c667dc00eb23fe906e4f7df6cbe1333379
rachleona <ractam2002@gmail.com>
2026-07-22 11:53:45 +0100

    Fixed resolve href issue

--- src/components/home/FaqContent.svelte
+++ src/components/home/FaqContent.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import type { ResolvedPathname } from '$app/types';
 	import type { RelatedLink } from '$data/home/faq';
 	import CircleChevronRight from '@iconify-svelte/lucide/circle-chevron-right';
 	const { related, answer }: { answer: string; related?: RelatedLink[] } = $props();
@@ -13,11 +14,13 @@
 			<div class="flex gap-2 items-center">
 				<div class="bg-primary font-semibold py-1 px-2 shrink-0">Related link</div>
 				<CircleChevronRight height="1rem" class="text-secondary" />
-				<a
-					href={link}
-					class="link link-info link-hover font-semibold"
-					rel={external ? 'external' : ''}>{label}</a
-				>
+				{#if external}
+					<a href={link} class="link link-info link-hover font-semibold" rel="external">{label}</a>
+				{:else}
+					<a href={link as ResolvedPathname} class="link link-info link-hover font-semibold"
+						>{label}</a
+					>
+				{/if}
 			</div>
 		{/each}
 	</div>
